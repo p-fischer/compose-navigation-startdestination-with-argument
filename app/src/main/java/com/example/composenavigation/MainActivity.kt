ad7ac763c1ceb3c749f5ac8bd0f63884541adc8f
Peter Fischer <p.fischer@anfe.ma>
2021-12-18 16:00:04 +0100

    Remove import artifacts

--- app/src/main/java/com/example/composenavigation/MainActivity.kt
+++ app/src/main/java/com/example/composenavigation/MainActivity.kt
@@ -3,12 +3,8 @@ package com.example.composenavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
